4a6f8cb584a4ef8d297191ff0e6b6ad0e346c3a5
b3m3 <b3m3@yandex.ru>
2022-06-10 13:36:11 +0300

    func createItem

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,14 @@
-import webp from './lib/webp.js';
-
 document.addEventListener('DOMContentLoaded', () => {
   'use strict';
 
-  webp();
+  const createItem = (wrapp, name, value) => {
+    const item = document.createElement('div');
+    item.classList.add('category__item', 'item-category');
+
+    item.innerHTML = `
+      <p class="item-category__name">${name}</p>
+      <input class='item-category__input item-input' type="number" value="${value}" />
+    `;
+    wrapp.append(item);
+  };
 });
